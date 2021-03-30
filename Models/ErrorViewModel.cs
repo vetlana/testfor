32ad3b91d968f1321099693c30bd2c91e4be2558
spike <spike@DESKTOP-A6SPSN1>
2021-03-30 18:19:43 +0800

    asdfsadf

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace 新建文件夹__2_.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
